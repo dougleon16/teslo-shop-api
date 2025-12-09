1c790f938a312bdce520706b56bd5a4c569d8447
Douglas leon <leondouglas1516@gmail.com>
2025-12-09 15:49:55 -0500

    install @nestjs/typeorm, pg, and typeorm packages for database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, //no se usa en production,
+    }),
+  ],
 })
 export class AppModule {}
